818eff464ad3267c6a5c5144f763e6b6ef9c78a9
Kara <kara.diggelmann@edu.tbz.ch>
2026-04-15 11:44:01 +0200

    basic project-structure fixed, methods and variables added

---
+++ src/snackautomat/Customer.java
@@ -0,0 +1,17 @@
+package snackautomat;
+
+public class Customer {
+    double insertedMoney;
+    boolean cancelled;
+
+
+    public void insertMoney(double insertedMoney) {}
+    public void selectProduct() {}
+    public void cancelPurchase() {}
+    public double getChange() { return 0; }
+    public void resetSession(){}
+    public void getInsertedMoney() {}
+    public boolean hasSufficientFunds() { return false; }
+
+
+}
